d5e3e9e8c22a3f7f01b2bc20de03c30e4dc47cf7
Henrriegel <luis.enrique.gr.legr@outlook.com>
2022-10-06 22:09:18 -0500

    Not authorized message added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,15 @@ class ApplicationController < ActionController::API
     def authenticate_request
         header = request.headers['Authorization']
         header = header.split(' ').last if header
+        if !header
+            render json: {
+                errorMessage: "Not authorized",
+                data: ""
+            }, status: :ok
+        end
         decoded = jwt_decode(header)
         current_user = User.find(decoded[:user_id]) rescue nil
-        if !current_user || !header
+        if !current_user
             render json: {
                 errorMessage: "Not authorized",
                 data: ""
